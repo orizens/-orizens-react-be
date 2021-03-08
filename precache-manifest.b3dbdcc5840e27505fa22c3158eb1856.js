d742fb40854fba6339c331d0e9b3c74cbf1ab12b
Oren Farhi <farhioren@gmail.com>
2021-03-07 19:13:02 -0500

    Updates

---
+++ precache-manifest.b3dbdcc5840e27505fa22c3158eb1856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74bb35d4797d336f967f6560b3c06cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f1992a37c5200f42860",
+    "url": "./static/css/main.9b28094c.chunk.css"
+  },
+  {
+    "revision": "6a5c00a796ad6517f1bc",
+    "url": "./static/js/2.42bf2adb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.42bf2adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1992a37c5200f42860",
+    "url": "./static/js/main.788ffbf8.chunk.js"
+  },
+  {
+    "revision": "079a371b4fd8c0369c5d",
+    "url": "./static/js/runtime-main.9e1fd83c.js"
+  }
+]);
